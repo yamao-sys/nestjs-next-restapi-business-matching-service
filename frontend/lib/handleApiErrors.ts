f2f7e54da9e49994f93cefb3786fd6c485c8b2f2
yamao-sys <yamaopanku@gmail.com>
2024-04-11 23:23:48 +0900

    エラーハンドリングの調整

--- frontend/lib/handleApiErrors.ts
+++ frontend/lib/handleApiErrors.ts
@@ -2,11 +2,15 @@ import { HTTPError } from '@aspida/fetch';
 import { redirect } from 'next/navigation';
 
 export const handleApiErrors = (error: HTTPError) => {
-	switch (error.response.status) {
-		case 401:
-			return redirect('/sign_in');
-		default:
-			console.error(error.response);
-			break;
-	}
+	if (error.response.status === 401) return redirect('/sign_in');
+	// switch (error.response.status) {
+	// 	case 401:
+	// 		return redirect('/sign_in');
+	// 	default:
+	// 		console.error(error.response);
+	// 		// 500ページを描画
+	// 		break;
+	// }
+
+	throw error;
 };
